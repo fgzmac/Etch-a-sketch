5a214f8f5b3d7abddb534dcc817e54ea67a50250
Gmac <fgzm16@gmail.com>
2025-02-16 22:10:44 -0800

    increased loop iterator to create divs that take up the whole screen.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector('#container');
  
-for(let i = 1; i <= 120; i++){
+for(let i = 1; i <= 6500; i++){
 const gridBoxes = document.createElement('div');
 gridBoxes.classList.add("gridBoxes");
-gridBoxes.style.height = "15px";
-gridBoxes.style.width = "15px";
+gridBoxes.style.height = "16px";
+gridBoxes.style.width = "16px";
 container.appendChild(gridBoxes);
 };
